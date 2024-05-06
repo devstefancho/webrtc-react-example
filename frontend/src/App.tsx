d3443734002335932bf4e1e1d602413f6c08a0c8
devstefancho <skku7714@gmail.com>
2024-05-07 01:01:45 +0900

    Connection with SDP (not exchange icecandidate)
    - 같은 ip인 device인 경우에는 영상공유가 됨

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,153 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+const iceConfiguration = {
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun1.l.google.com:19302" },
+  ],
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const localVideoRef = useRef<HTMLVideoElement>(null);
+  const remoteVideoRef = useRef<HTMLVideoElement>(null);
+  const [pc, setPeerConnection] = useState<RTCPeerConnection | null>(null);
+  const [offererSDP, setOffererSDP] = useState<string>("");
+  const [answererSDP, setAnswererSDP] = useState<string>("");
+
+  useEffect(() => {
+    const init = async () => {
+      const pc = new RTCPeerConnection(iceConfiguration);
+      setPeerConnection(pc);
+
+      pc.ontrack = async (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+          const remoteStream = new MediaStream();
+          remoteStream.addTrack(track);
+          remoteVideoRef.current!.srcObject = remoteStream;
+        });
+      };
+
+      pc.onicecandidate = (event) => {
+        console.log("onicecandidate", event);
+      };
+    };
+
+    init();
+
+    return () => {
+      if (pc) {
+        pc.close();
+      }
+    };
+  }, []);
+
+  const handleLocalStream = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
+    localVideoRef.current!.srcObject = stream;
+  };
+
+  const handleAddTrackToPeerConnection = async () => {
+    const stream = localVideoRef.current!.srcObject as MediaStream;
+    if (!stream) {
+      return alert("Local Stream is not available");
+    }
+
+    if (pc) {
+      stream.getTracks().forEach((track) => {
+        pc.addTrack(track, stream);
+      });
+    }
+  };
+
+  const createOffer = async () => {
+    if (!pc) return;
+
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+  };
+
+  const getOffer = async () => {
+    if (!pc) return;
+    setOffererSDP(JSON.stringify(pc.localDescription));
+  };
+
+  const createAnswer = async () => {
+    if (!pc) return;
+
+    const offer = JSON.parse(offererSDP);
+    await pc.setRemoteDescription(offer);
+    const answer = await pc.createAnswer();
+    await pc.setLocalDescription(answer);
+  };
+
+  const getAnswer = async () => {
+    if (!pc) return;
+    setAnswererSDP(JSON.stringify(pc.localDescription));
+  };
+
+  const addAnswer = async () => {
+    if (pc && !pc.currentRemoteDescription) {
+      const answer = JSON.parse(answererSDP);
+      await pc.setRemoteDescription(answer);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Hello WebRTC</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div className="flex flex-col gap-[10px]">
+          <video
+            key="localVideo"
+            id="localVideo"
+            ref={localVideoRef}
+            className="w-[300px] h-[150px] border-white border-2 m-[10px]"
+            autoPlay
+            playsInline
+            loop
+            muted
+          ></video>
+          <video
+            key="remoteVideo"
+            id="remoteVideo"
+            ref={remoteVideoRef}
+            className="w-[300px] h-[150px] border-white border-2 m-[10px]"
+            autoPlay
+            playsInline
+            loop
+            muted
+          ></video>
+        </div>
+        <div className="flex flex-col gap-[10px]">
+          <button onClick={handleLocalStream}>Get My Local Stream</button>
+          <button onClick={handleAddTrackToPeerConnection}>
+            Add track to PeerConnection
+          </button>
+          <button onClick={createOffer}>(A) Create offer</button>
+          <button onClick={getOffer}>(A) offer</button>
+          <input
+            type="text"
+            value={offererSDP}
+            onChange={(e) => {
+              setOffererSDP(e.target.value);
+            }}
+          />
+          <button onClick={createAnswer}>(B) Create Answer</button>
+          <button onClick={getAnswer}>(B) Answer</button>
+          <input
+            type="text"
+            value={answererSDP}
+            onChange={(e) => {
+              setAnswererSDP(e.target.value);
+            }}
+          />
+          <button onClick={addAnswer}>(A) Add Answer</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
